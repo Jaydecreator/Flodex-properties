05d8c75149ed760e62c43636c946c17fba221722
Njabulo Alx <ndlovunjabulo17@gmail.com>
2024-09-03 16:13:30 +0200

    creating user Update API

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 import dontenv from "dotenv";
 import userRouter from "./routes/user.route.js";
 import authRouter from "./routes/auth.route.js";
+import cookieParser from 'cookie-parser';
 dontenv.config();
 
 mongoose
@@ -17,6 +18,8 @@ mongoose
 const app = express();
 
 app.use(express.json());
+
+app.use(cookieParser());
 
 app.listen(3000, () => {
   console.log("Server running on port 3000! puta");
