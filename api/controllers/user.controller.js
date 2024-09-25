85509b449fd6af9193728f1568de1a877b6a72a5
Njabulo Alx <ndlovunjabulo17@gmail.com>
2024-09-25 14:08:23 +0200

    Create get listing api route done

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,4 +1,5 @@
 import bcryptjs from "bcryptjs";
+import Listing from "../models/listing.model.js";
 import User from "../models/user.model.js";
 import { errorHandler } from "../utills/error.js";
 
@@ -46,5 +47,18 @@ export const deleteUser = async (req, res, next) => {
     res.status(200).json("User has been deleted EXCOMUNINCANO!..");
   } catch (error) {
     next(error);
+  }
+};
+
+export const getUserListings = async (req, res, next) => {
+  if (req.user.id === req.params.id) {
+    try {
+      const listing = await Listing.find({userRef: req.params.id});
+      res.status(200).json(listings);
+    } catch (error) {
+      next(error)
+    }
+  } else {
+    return next(errorHandler(401, "You can only veiw your own lising"));
   }
 };
