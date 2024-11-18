433f871dfc7923435d708205ba1f6667ef74ae6d
Njabulo Alx <ndlovunjabulo17@gmail.com>
2024-11-18 20:43:20 +0200

    Header Search functionality Complete

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,9 +1,30 @@
 /* eslint-disable no-unused-vars */
+import {FaSearch} from 'react-icons/fa';
 import { IoSearchCircleOutline } from "react-icons/io5";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
+import { useEffect, useState } from "react";
 export default function Header() {
   const { currentUser } = useSelector((state) => state.user);
+  const [searchTerm, setSearchTerm] = useState('');
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const urlParams = new URLSearchParams(window.location.search);
+    urlParams.set('searchTerm', searchTerm);
+    const searchQuery = urlParams.toString();
+    navigate(`/search?${searchQuery}`);
+  };
+
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const searchTermFromUrl = urlParams.get('searchTerm');
+    if (searchTermFromUrl) {
+      setSearchTerm(searchTermFromUrl);
+    }
+  }, [location.search]);
+
   return (
     <header className="bg-slate-200 shadow-md">
       <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
@@ -14,15 +35,20 @@ export default function Header() {
           </h1>
         </Link>
 
-        <form className="bg-slate-100 p-3 rounded-lg flex items-center">
+        <form onSubmit={handleSubmit} className="bg-slate-100 p-3 rounded-lg flex items-center">
           <input
             type="text"
             placeholder="Search..."
             className="bg-transparent focus: outline-none w-24 sm:w-64"
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
           />
           {/* download and install npm i react-icons  to veiw icons*/}
-          {/* <FaSearch className='text-slate-600' /> */}
-          <IoSearchCircleOutline className="text-slate-600 size-7" />
+          <button>
+            <FaSearch className='text-slate-600' />
+          </button>
+          
+          {/* <IoSearchCircleOutline className="text-slate-600 size-7" /> */}
         </form>
 
         <ul className="flex gap-4">
